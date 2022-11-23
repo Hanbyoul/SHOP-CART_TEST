f9b455fdbdc26271a44bd8b4cf1fa478f07dc894
Hanbyoul <dytjqdla120@naver.com>
2022-11-24 03:03:54 +0900

    second

--- src/redux/actions/productAction.js
+++ src/redux/actions/productAction.js
@@ -3,7 +3,7 @@ import { productActions } from "../reducers/productReducer";
 function getProducts() {
   return async (dispatch) => {
     let url =
-      "https://my-json-server.typicode.com/Hanbyoul/hanbyoul-shop/productItems";
+      "https://my-json-server.typicode.com/Hanbyoul/hanbyoul-shop/products";
     let res = await fetch(url);
     let data = await res.json();
     data.sort((a, b) => b.score - a.score);
